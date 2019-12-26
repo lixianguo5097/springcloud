fccc23c75c8af2f71defd7d5368e0939bd23509e
李先国 <ied_lxg@gdhqx.com>
2019-12-26 15:05:48 +0800

    eureka集群

---
+++ springcloud-eureka/springboot-eureka-server1/src/main/java/com/lxg/EurekaServerApp.java
@@ -0,0 +1,17 @@
+package com.lxg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author LXG
+ * @date 2019-12-25
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApp.class, args);
+    }
+}
