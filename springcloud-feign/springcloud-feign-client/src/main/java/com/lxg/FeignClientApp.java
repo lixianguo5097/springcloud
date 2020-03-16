2541270f97950fa26b67e282d1203e1f97d6ac16
李先国 <ied_lxg@gdhqx.com>
2020-03-16 16:50:09 +0800

    集成Hystrix Dashboard仪表盘

--- springcloud-feign/springcloud-feign-client/src/main/java/com/lxg/FeignClientApp.java
+++ springcloud-feign/springcloud-feign-client/src/main/java/com/lxg/FeignClientApp.java
@@ -2,16 +2,21 @@ package com.lxg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
+ * 访问http://localhost:8102/hystrix 打开熔断器监控首页
  * @author LXG
  * @date 2019-12-31
  */
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class FeignClientApp {
     public static void main(String[] args) {
         SpringApplication.run(FeignClientApp.class, args);
